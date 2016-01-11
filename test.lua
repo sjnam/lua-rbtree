cd47c228f231ec05ace76e3c5c3053eefd1a89d5
Soojin Nam <jsunam@gmail.com>
2016-01-11 22:16:25 +0900

    test

--- test.lua
+++ test.lua
@@ -1,18 +1,24 @@
-local rbtree = require "rbtree"
 local floyd = require "floyd"
+local rbtree = require "rbtree"
 
+local ipairs = ipairs
+local permgen = floyd.permgen
+local shuffle = floyd.shuffle
+
+
+local N = 10000
 
 local tree = rbtree.new()
 
 local node
-local arr = floyd.permgen(100000, 1000000)
+local arr = permgen(N, 1000000)
 for _, v in ipairs(arr) do
    tree:insert(v)
 end
 
 print("INSERT OK")
 
-local idx = floyd.shuffle(100000)
+local idx = shuffle(N)
 for _, v in ipairs(idx) do
    tree:delete(arr[v])
 end
