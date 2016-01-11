81a8e8b1770eb20678b4eb23f16e1f01c8391f85
Soojin Nam <jsunam@gmail.com>
2016-01-11 22:22:01 +0900

    delete floyd

--- test.lua
+++ test.lua
@@ -1,9 +1,31 @@
-local floyd = require "floyd"
 local rbtree = require "rbtree"
 
-local ipairs = ipairs
-local permgen = floyd.permgen
-local shuffle = floyd.shuffle
+-- Floyd's random permutation generator
+local function permgen (m, n)
+   local s, head = {}
+   for j = n-m+1, n do
+      local t = math.random(j)
+      if not s[t] then
+         head = { key = t, next = head }
+         s[t] = head
+      else
+         local curr = { key = j, next = s[t].next }
+         s[t].next = curr
+         s[j] = curr
+      end
+   end
+
+   s = {}
+   while head do
+      table.insert(s, head.key)
+      head = head.next
+   end
+   
+   return s
+end
+
+
+math.randomseed(os.time())
 
 
 local N = 10000
@@ -18,7 +40,7 @@ end
 
 print("INSERT OK")
 
-local idx = shuffle(N)
+local idx = permgen(N, N)
 for _, v in ipairs(idx) do
    tree:delete(arr[v])
 end
